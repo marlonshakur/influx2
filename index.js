97a44e72f57709e6883222abf1c7017d2049cee5
unknown <Administrator@mydomain.local>
2021-01-16 12:54:44 +0200

    this is modifications up to chapter 5

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const { static } = require('express')
 const express = require('express')
 const path = require('path')
 const app = express()
+const ejs = require('ejs')
+app.set('view engine','ejs')
 app.use(static('./public'))
 app.listen(4000)
+
 const homepage = path.resolve(__dirname,'./views/index.html')
 const about = path.resolve(__dirname,'./views/about.html')
 const post = path.resolve(__dirname,'./views/post.html')
@@ -11,18 +14,18 @@ const contact = path.resolve(__dirname,'./views/contact.html')
 
 app.get('/',(req,res)=>{
 console.log('the user requested : '+req.url)
-res.sendFile(homepage)
+res.render('index')
     })
 app.get('/about',(req,res)=>{
     console.log('the user requested : '+req.url)
-    res.sendFile(about)
+    res.render('about')
     })
 app.get('/post',(req,res)=>{
     console.log('the user requested : '+req.url)
-    res.sendFile(post)
+    res.render('post')
     })
 app.get('/contact',(req,res)=>{
     console.log('the user requested : '+req.url)
-    res.sendFile(contact)
+    res.render('contact')
     })
         

a1eedfddbb161254840a1eb2d7e7f7a73fb9d9aa
unknown <Administrator@mydomain.local>
2021-01-19 11:14:00 +0200

    before mvc

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fileUpload = require('express-fileupload')
 const mongoose = require('mongoose')
 const blogpost = require('./models/blogpost')
 const {
@@ -6,22 +7,39 @@ const {
 const express = require('express')
 const path = require('path')
 const app = express()
+
 const ejs = require('ejs')
 const bodyParser =
-require('body-parser')
+    require('body-parser')
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 const {
     use
 } = require('browser-sync')
-const { data } = require('jquery')
+const {
+    data
+} = require('jquery')
 app.set('view engine', 'ejs')
 app.use(static('./public'))
+app.use(fileUpload())
+const validateMiddleWare =
+(
+req,res,next)=>{
+if(req.files == null || req.body.title == null || req.body.title == null){
+return res.redirect('/posts/new')
+}
+next()
+}
+app.use('/posts/store',validateMiddleWare)
 // CONNECTION TO MONGODB DATABASE CLOUD SERVER 
 const connectionstring = 'mongodb+srv://amr:amr123@cluster0.v8k3k.mongodb.net/influx?retryWrites=true&w=majority'
 mongoose.connect(connectionstring, {
     useNewUrlParser: true
-},{ useUnifiedTopology: true }, (err, data) => {
+}, {
+    useUnifiedTopology: true
+}, (err, data) => {
     console.log(err)
 })
 app.listen(4000)
@@ -31,73 +49,89 @@ const post = path.resolve(__dirname, './views/post.html')
 const contact = path.resolve(__dirname, './views/contact.html')
 
 app.get('/', async (req, res) => {
-    const blogposts = await blogpost.find({}, function(err,data){})
+    const blogposts = await blogpost.find({}, function (err, data) {})
     console.log('the user requested : ' + req.url)
-    res.render('index',{blogposts:blogposts})
+    res.render('index', {
+        blogposts: blogposts
+    })
 })
 app.get('/about', (req, res) => {
     console.log('the user requested : ' + req.url)
     res.render('about')
 })
-app.get('/post',(req,res)=>{
+app.get('/post', (req, res) => {
     res.render('sample')
 })
-app.get('/post/:id', async(req, res) => {
-const blogpostss= await blogpost.findById((req.params.id),(err,x)=>{
-console.log(err,x)
-})
-res.render('post',{blogpostss:blogpostss})
+app.get('/post/:id', async (req, res) => {
+    const blogpostss = await blogpost.findById((req.params.id), (err, x) => {
+        console.log(err, x)
+    })
+    res.render('post', {
+        blogpostss: blogpostss
+    })
 })
 app.get('/contact', (req, res) => {
     console.log('the user requested : ' + req.url)
     res.render('contact')
 })
 
-app.get('/posts/new',(req,res)=>{
+app.get('/posts/new', (req, res) => {
     console.log(req.url)
     res.render('create')
 })
-app.post('/posts/store',async(req,res)=>{
-    await blogpost.create(req.body,(err,data)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.log(data)
-        }
-    })
-    res.redirect('/')
-    })
+app.post('/posts/store', (req, res) => {
+            let image = req.files.image;
+            image.mv(path.resolve(__dirname,'public/img', image.name), async () => {
+                            await blogpost.create({...req.body,image: '/img/' + image.name}, (err, data) => {
+                                if (err) {
+                                    console.log(err)
+                                } 
+                            })
+                            res.redirect('/')
+                        }
 
-        //INDEX - show all campgrounds
-app.get("/search", (req, res)=>{
-    var noMatch = null;
-    console.log(req.query)
-    if(req.query.title) {
-        const regex = new RegExp(escapeRegex(req.query.title), 'gi');
-        // Get all campgrounds from DB
-        const searchquery = blogpost.find({body: regex}, function(err, blogpostsQ){
-           if(err){
-               console.log(err);
-           } else {
-              if(blogpostsQ.length < 1) {
-                  noMatch = "No posts match that query, please try again.";
-              }
-              res.render("search",{blogpostsQ:blogpostsQ, noMatch: noMatch});
-              console.log(blogpostsQ)
-           }
-        });
-    } else {
-        // Get all campgrounds from DB
-        blogpost.find({}, function(err, blogpostsQ){
-           if(err){
-               console.log(err);
-           } else {
-              res.render("search",{blogpostsQ:blogpostsQ, noMatch: noMatch});
-           }
-        });
-    }
-});
+                    )})
 
-function escapeRegex(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-};
+
+
+                    //INDEX - show all campgrounds
+                    app.get("/search", (req, res) => {
+                        var noMatch = null;
+                        console.log(req.query)
+                        if (req.query.title) {
+                            const regex = new RegExp(escapeRegex(req.query.title), 'gi');
+                            // Get all campgrounds from DB
+                            const searchquery = blogpost.find({
+                                body: regex
+                            }, function (err, blogpostsQ) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    if (blogpostsQ.length < 1) {
+                                        noMatch = "No posts match that query, please try again.";
+                                    }
+                                    res.render("search", {
+                                        blogpostsQ: blogpostsQ,
+                                        noMatch: noMatch
+                                    });
+                                    console.log(blogpostsQ)
+                                }
+                            });
+                        } else {
+                            // Get all campgrounds from DB
+                            blogpost.find({}, function (err, blogpostsQ) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    res.render("search", {
+                                        blogpostsQ: blogpostsQ,
+                                        noMatch: noMatch
+                                    });
+                                }
+                            });
+                        }
+                    });
+
+                    function escapeRegex(text) {
+                        return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+                    };
--- models/blogpost.js
+++ models/blogpost.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose')
 const schema = mongoose.Schema;
+//------------------------------------------------
 const blogpostschema = new schema({
 title: String,
 body: String,
 name: String,
-datePosted:{ /* can declare property type with an object like this because we need 'default' */
+datePosted:{
 type: Date,
 default: new Date()
-}
+},
+image: String
 });
-
+//------------------------------------------------
 const blogpost = mongoose.model('blogpost',blogpostschema);
 module.exports = blogpost
 
